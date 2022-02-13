cda5962493f9efb8980f67ab7c3dcf5ce2cdadb2
Jordy <justjordyy01@gmail.com>
2022-02-13 22:37:04 +0100

    Datapush
    Pushing data to proceed work on a different machine

--- profile.php
+++ profile.php
@@ -18,7 +18,8 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-            <link rel="stylesheet" type="text/css" href="./css/index.css">
+            <!-- <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"> -->
+            <link rel="stylesheet" type="text/css" href="./css/profile.css">
             <title>CMS Aplication</title>
         </head>
     <body>
@@ -39,7 +40,7 @@
         ?>
         
         <div class="float-md-right c" id="usercard">
-            <div class="card" style="width: 9rem;">
+            <div class="card" style="width: 9.5rem;">
                 <div class="card-body">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
                     <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
@@ -76,7 +77,20 @@
                     </div>
                 </div>
             </div>
-        </div>    
+        </div>
+        <div class="infopos">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
     <script>
         function showblock(){
